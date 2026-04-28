49c3cfabef8b3839048ab7850baf45d6dcef910f
amira <amiraahriz005@gmail.com>
2026-04-28 10:50:55 +0000

    Modif auth

--- clientflow/settings.py
+++ clientflow/settings.py
@@ -96,20 +96,8 @@ DATABASES = {
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
-AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
-]
+# À faire uniquement pour le développement
+AUTH_PASSWORD_VALIDATORS = []
 
 
 # Internationalization
